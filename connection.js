23e737554497dcf2664b1f81486b6149b99d352b
arpitsrivastava54 <139012509+arpitsrivastava54@users.noreply.github.com>
2023-07-13 00:07:49 +0530

    Add files via upload

---
+++ connection.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+try {
+    mongoose.connect(process.env.DATABASE_URL)
+    console.log("connection establish from database...");
+    
+} catch (error) {
+    console.log("error while connecting from database !!!")
+}
