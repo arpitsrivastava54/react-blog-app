6ec051e972befc30009f3038dcad67070af1a1aa
arpitsrivastava54 <139012509+arpitsrivastava54@users.noreply.github.com>
2023-07-15 15:24:35 +0530

    Delete connection.js

--- connection.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-try {
-    mongoose.connect(process.env.DATABASE_URL)
-    console.log("connection establish from database...");
-    
-} catch (error) {
-    console.log("error while connecting from database !!!")
-}
